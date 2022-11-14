003c2286662b3191430b091ea56b95b847aff64e
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-11-15 04:00:19 +0500

    icons displayed

--- SeerahtBook/src/navigation/AppNavigation.js
+++ SeerahtBook/src/navigation/AppNavigation.js
@@ -12,6 +12,11 @@ import DownloadCourse from '../Frontend/DownloadCourse/DownloadCourse'
 import ConnectWithSeeraht from '../Frontend/ConnectWithSeeraht/ConnectWithSeeraht'
 import AboutSeeraht from '../Frontend/AboutSeeraht/AboutUs'
 import HelpSeeraht from '../Frontend/HelpSeeraht/HelpSeeraht'
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Octicons from "react-native-vector-icons/Octicons";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import FontAwesome5Pro from "react-native-vector-icons/FontAwesome5Pro";
+
 
 
 const Drawer = createDrawerNavigator();
@@ -24,54 +29,97 @@ export default function AppNavigation() {
             <Drawer.Navigator initialRouteName="bachonkiNamaz"
             >
                 <Drawer.Screen name='bachonkiNamaz' component={Home}
-
                     options={{
                         headerTitle: "بچیوں کی نماز",
                         headerTitleAlign: "center",
                         title: "Bachon Ki Namaz",
+                        drawerIcon: () => (
+                            <MaterialIcons name="menu-book" color="#1d5968" size={18}
+                            />
+                        ),
                     }}
 
                 />
                 <Drawer.Screen name='buyNow' component={BuyNow}
                     options={{
                         title: "Buy Now",
+                        drawerIcon: () => (
+                            <MaterialIcons name="shopping-cart" color="#1d5968" size={18}
+                            />
+                        ),
                     }}
                 />
                 <Drawer.Screen name='connectWithSeeraht' component={ConnectWithSeeraht}
                     options={{
                         title: "Connect With Seeraht",
+                        drawerIcon: () => (
+                            <Octicons name="link" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='aboutSeeraht' component={AboutSeeraht}
                     options={{
                         title: "About Seeraht",
+                        drawerIcon: () => (
+                            <MaterialIcons name="info" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='download' component={Download}
                     options={{
                         title: "Download",
+                        drawerIcon: () => (
+                            <MaterialIcons name="menu-book" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='helpSeeraht' component={HelpSeeraht}
                     options={{
                         title: "Help Seeraht",
+                        drawerIcon: () => (
+                            <FontAwesome5 name="hands-helping" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='moreApps' component={MoreApps}
                     options={{
                         title: "More Apps",
+                        drawerIcon: () => (
+                            <FontAwesome5Pro name="google-play" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='subscribeForWhatsApp' component={Subscribes}
                     options={{
                         title: "Subscribe For WhatsApp",
+                        drawerIcon: () => (
+                            <FontAwesome5Pro name="whatsapp" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='share' component={Share}
                     options={{
                         title: "Share",
+                        drawerIcon: () => (
+                            <FontAwesome5 name="share-alt" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='submitFeedback' component={Feedback}
                     options={{
                         title: "Submit Feedback",
+                        drawerIcon: () => (
+                            <MaterialIcons name="feedback" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
                 <Drawer.Screen name='downloadCourse' component={DownloadCourse}
                     options={{
                         title: "Download Course",
+                        drawerIcon: () => (
+                            <FontAwesome5 name="file-download" color="#1d5968" size={18}
+                            />
+                        ),
                     }} />
 
             </Drawer.Navigator>
