9de382e66b23ce18822b7d8536e5efcde1b6547f
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-11-16 22:40:54 +0500

    Update ConnectWithSeeraht.js

--- SeerahtBook/src/Frontend/ConnectWithSeeraht/ConnectWithSeeraht.js
+++ SeerahtBook/src/Frontend/ConnectWithSeeraht/ConnectWithSeeraht.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, Linking } from 'react-native'
+import { View, StyleSheet, Linking } from 'react-native'
 import { Button } from 'react-native-paper'
 import FontAwesome5Brands from 'react-native-vector-icons/FontAwesome5Pro'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
@@ -17,10 +17,10 @@ export default function ConnectWithSeeraht() {
                     />
                 )} onPress={() => Linking.openURL("https://www.facebook.com/SEERAHT")}
 
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
                 >
                 </Button>
                 <Button icon={() => (
@@ -30,10 +30,10 @@ export default function ConnectWithSeeraht() {
                         color="#FF000D"
                     />
                 )} onPress={() => Linking.openURL('https://www.instagram.com/alSEERAHT/')}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
 
                 >
                 </Button>
@@ -44,66 +44,66 @@ export default function ConnectWithSeeraht() {
                         color="#00acee"
                     />
                 )} onPress={() => Linking.openURL('https://twitter.com/MuftiMustafaA?t=oTL37R10sA_gW1m_lx6hPg&s=09')}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
-
-                >
-                </Button>
-                <Button icon={() => (
-                     <FontAwesome5Brands
-                     name='youtube'
-                     size={35}
-                     color="#FF0202"
-                 />
-                )} onPress={() => Linking.openURL("https://youtube.com/c/MuftiMustafaAziz")}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
-
-                >
-                </Button>
-                <Button icon={() => (
-                   <MaterialCommunityIcons
-                   name='web'
-                   size={35}
-                //    color="#FF0202"
-               />
-                )} onPress={() => Linking.openURL("https://seeraht.com/")}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
 
                 >
                 </Button>
                 <Button icon={() => (
                     <FontAwesome5Brands
-                    name='whatsapp'
-                    size={35}
-                    color="#128c7e"
-                />
-                )} onPress={() => alert('Pressed')}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
+                        name='youtube'
+                        size={35}
+                        color="#FF0202"
+                    />
+                )} onPress={() => Linking.openURL("https://youtube.com/c/MuftiMustafaAziz")}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
 
                 >
                 </Button>
                 <Button icon={() => (
                     <MaterialCommunityIcons
-                    name='phone-dial'
-                    size={35}
-                    color="#5602FF"
-                />
+                        name='web'
+                        size={35}
+                    //    color="#FF0202"
+                    />
+                )} onPress={() => Linking.openURL("https://seeraht.com/")}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
+
+                >
+                </Button>
+                <Button icon={() => (
+                    <FontAwesome5Brands
+                        name='whatsapp'
+                        size={35}
+                        color="#128c7e"
+                    />
                 )} onPress={() => alert('Pressed')}
-                style={{
-                    paddingRight:6,
-                    marginRight: -25,
-                }}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
+
+                >
+                </Button>
+                <Button icon={() => (
+                    <MaterialCommunityIcons
+                        name='phone-dial'
+                        size={35}
+                        color="#5602FF"
+                    />
+                )} onPress={() => alert('Pressed')}
+                    style={{
+                        paddingRight: 6,
+                        marginRight: -25,
+                    }}
                 >
                 </Button>
             </View>
