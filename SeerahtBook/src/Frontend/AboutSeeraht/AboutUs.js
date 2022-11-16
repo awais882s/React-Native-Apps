6ef0718d13cf9f4d10609435593992cbb244d711
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-11-16 22:14:47 +0500

    Contact Pages Icon is done

--- SeerahtBook/src/Frontend/AboutSeeraht/AboutUs.js
+++ SeerahtBook/src/Frontend/AboutSeeraht/AboutUs.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 
 export default function AboutSeeraht() {
   return (
-    <View>
-      <Text>AboutSeeraht</Text>
-    </View>
+    <ScrollView>
+      <View style={Styles.container}>
+        <View>
+        <Text style={Styles.header}>Mutfi Mustafa Aziz</Text>
+        </View>
+        <View>
+        <Text style={Styles.header}>Seeraht Institute & Research Center</Text>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
+
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "#F0E4E4E9",
+    fontSize: 15,
+    color: "#000000",
+    padding:5,
+    paddingLeft:20
+  }
+})
