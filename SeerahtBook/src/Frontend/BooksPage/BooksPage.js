efd337a56d4b40e01b4217051773b2299ca6a0d4
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-11-20 20:10:09 +0500

    Update BooksPage.js

--- SeerahtBook/src/Frontend/BooksPage/BooksPage.js
+++ SeerahtBook/src/Frontend/BooksPage/BooksPage.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import { View } from 'react-native'
-import { StyleSheet, Dimensions } from 'react-native';
-import Pdf from 'react-native-pdf';
+
+
 export default function BooksPage() {
     const source = require('../../Assest/pdfbook.pdf');
     return (
