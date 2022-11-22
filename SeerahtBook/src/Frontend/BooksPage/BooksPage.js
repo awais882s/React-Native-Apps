352a785973410ad75316d6db2dc4950f521b7c40
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2022-11-22 12:30:14 +0500

    changes in code

--- SeerahtBook/src/Frontend/BooksPage/BooksPage.js
+++ SeerahtBook/src/Frontend/BooksPage/BooksPage.js
@@ -6,18 +6,6 @@ export default BooksPage = () => {
                 horizontal={true}
                 source={source}
                 trustAllCerts={false}
-                onLoadComplete={(numberOfPages, filePath) => {
-                    console.log(`Number of pages: ${numberOfPages}`);
-                }}
-                onPageChanged={(page, numberOfPages) => {
-                    console.log(`Current page: ${page}`);
-                }}
-                onError={(error) => {
-                    console.log(error);
-                }}
-                onPressLink={(uri) => {
-                    console.log(`Link pressed: ${uri}`);
-                }}
                 style={styles.pdf} />
         </View>
     )
