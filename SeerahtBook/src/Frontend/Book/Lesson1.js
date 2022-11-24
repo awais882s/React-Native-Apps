ce5a5456aa33cabcd86aef3ce1e8daa409ce8e65
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-11-24 13:09:15 +0500

    storages

--- SeerahtBook/src/Frontend/Book/Lesson1.js
+++ SeerahtBook/src/Frontend/Book/Lesson1.js
@@ -1,11 +1,12 @@
-import React, { useContext, useState } from 'react';
-import { StyleSheet, Dimensions, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Dimensions, View } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
 import Pdf from 'react-native-pdf';
 import { useAuthContext } from '../../Context/Context';
 
 export default Lesson1 = () => {
     let { pageInfo } = useAuthContext()
-    const source = require('../../Assest/pdfbook.pdf');  // ios only
+    const source = { uri: "https://firebasestorage.googleapis.com/v0/b/seeraht-book.appspot.com/o/pdfbook.pdf?alt=media&token=f35b6899-e357-474", cache: true };  // ios only
     return (
         <View style={styles.container}>
             <Pdf
